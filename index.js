ea1ee70deaf12354da00a7aa98c12c79dcc15d86
lebzylee <118142087+lebzylee@users.noreply.github.com>
2022-11-21 11:48:38 -0800

    Add club website files

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.faq-des-names-1').addEventListener('click' ,shownav);
+
+
+
+function shownav(){
+    document.querySelector('.faq-des-des').classList.toggle(show-faq-des-des);
+}
